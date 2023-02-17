6209e1a89b6f7d2530df72e509bc9d13692b421d
cbk914 <cbk914@riseup.net>
2023-02-17 17:44:49 +0100

    Update nmap.cpp

--- nmap.cpp
+++ nmap.cpp
@@ -6,6 +6,45 @@ const char g_szClassName[] = "myWindowClass";
 const int g_nWidth = 400;
 const int g_nHeight = 250;
 
+// Step 1: Registering the Window Class
+WNDCLASSEX wc;
+ZeroMemory(&wc, sizeof(wc));
+wc.cbSize = sizeof(wc);
+wc.style = 0;
+wc.lpfnWndProc = WndProc;
+wc.cbClsExtra = 0;
+wc.cbWndExtra = 0;
+wc.hInstance = hInstance;
+wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+wc.lpszMenuName = NULL;
+wc.lpszClassName = g_szClassName;
+wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+if (!RegisterClassEx(&wc)) {
+    MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+    return 0;
+}
+
+// Step 2: Creating the Window
+HWND hwnd = CreateWindowEx(
+    WS_EX_CLIENTEDGE,
+    g_szClassName,
+    "Nmap GUI",
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT, g_nWidth, g_nHeight,
+    NULL, NULL, hInstance, NULL);
+
+if (hwnd == NULL) {
+    MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+    return 0;
+}
+
+// Step 3: Show the Window
+ShowWindow(hwnd, nCmdShow);
+UpdateWindow(hwnd);
+
+
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -122,43 +161,6 @@ return 0;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-// Step 1: Registering the Window Class
-WNDCLASSEX wc;
-ZeroMemory(&wc, sizeof(wc));
-wc.cbSize = sizeof(wc);
-wc.style = 0;
-wc.lpfnWndProc = WndProc;
-wc.cbClsExtra = 0;
-wc.cbWndExtra = 0;
-wc.hInstance = hInstance;
-wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-wc.lpszMenuName = NULL;
-wc.lpszClassName = g_szClassName;
-wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-if (!RegisterClassEx(&wc)) {
-    MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-    return 0;
-}
-
-// Step 2: Creating the Window
-HWND hwnd = CreateWindowEx(
-    WS_EX_CLIENTEDGE,
-    g_szClassName,
-    "Nmap GUI",
-    WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT, CW_USEDEFAULT, g_nWidth, g_nHeight,
-    NULL, NULL, hInstance, NULL);
-
-if (hwnd == NULL) {
-    MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-    return 0;
-}
-
-// Step 3: Show the Window
-ShowWindow(hwnd, nCmdShow);
-UpdateWindow(hwnd);
 
 // Step 5: The Message Loop
 MSG msg;
